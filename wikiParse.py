7f14e8f2fd726f9b03dfbace0cbe798ea96470ba
Kimball Wu ( 吳駿盛 ) <KimballWu@taiwanmobile.com>
2016-07-19 11:04:54 +0800

    temp version of wikiParse.py

--- wikiParse.py
+++ wikiParse.py
@@ -10,12 +10,22 @@ import pandas as pd
 xml='textmine/corpus/zhwiki-20160701-pages-articles-multistream.xml'
 #%%
 tagN=0
-level=0        
+level=0
+order=0        
 elementList=[]
+levelList=[]
+orderList=[]
 for event, tag in etree.iterparse(xml, events=['start','end']):
     if tagN<1000:
         tagN +=1
-        elementList.append(tag) 
+        if event=='start':
+            level+=1
+            order+=1
+            levelList.append(level)
+        elif event=='end':
+            level-=1    
+            order+=1                
+            elementList.append(tag)
         continue
     else:
         break      
@@ -23,12 +33,16 @@ for event, tag in etree.iterparse(xml, events=['start','end']):
 textList=[x.text for x in elementList]
 tagList=[x.tag.split('}')[1] for x in elementList] 
 attrList=[x.attrib for x in elementList]
-data=pd.DataFrame([tagList, textList, attrList]).T
-data.columns=['tag','text', 'attrList']
-data['size']=data['text'].apply(lambda x: len(x) if x!=None else 0)
-data=data.sort_values(by=['size'],ascending=False)
-data=['key']
+data=pd.DataFrame([tagList,levelList,textList, attrList]).T
+data.columns=['tag','level','text', 'attrList']
+data['size']=data['text'].apply(lambda x: len(unicode(x)) if x!=None else 0)
+#data=data.sort_values(by=['size'],ascending=False)
 cDict=data['tag'].value_counts()
+
+
+#%%
+
+read=
 
 
 #%%
