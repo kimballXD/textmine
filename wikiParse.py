9e041e6246c6b09748f74c5289d1c6f06f393d8c
Kimball Wu ( 吳駿盛 ) <KimballWu@taiwanmobile.com>
2016-07-19 16:22:37 +0800

    wikiparse ver0.1

--- wikiParse.py
+++ wikiParse.py
@@ -1,58 +1,55 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jul 18 15:31:13 2016
-
+    #setUpDB
+    #for x in range(postSection):
+    #    getPageList()
+    #    parsePageList()
+    #    cleanText()
+    #    commitToDB()
 @author: KimballWu
 """
 
 import xml.etree.ElementTree as etree
 import pandas as pd
+
 xml='textmine/corpus/zhwiki-20160701-pages-articles-multistream.xml'
+nameSpace='http://www.mediawiki.org/xml/export-0.10/'
 #%%
-tagN=0
-level=0
-order=0        
-elementList=[]
-levelList=[]
-orderList=[]
-for event, tag in etree.iterparse(xml, events=['start','end']):
-    if tagN<1000:
-        tagN +=1
-        if event=='start':
-            level+=1
-            order+=1
-            levelList.append(level)
-        elif event=='end':
-            level-=1    
-            order+=1                
-            elementList.append(tag)
-        continue
+
+def fullTag(tag,nameSpace=nameSpace,prefix=None):
+    result='{{{}}}{}'.format(nameSpace,tag)
+    if prefix!=None:
+        return prefix+result
     else:
-        break      
-#%%    
-textList=[x.text for x in elementList]
-tagList=[x.tag.split('}')[1] for x in elementList] 
-attrList=[x.attrib for x in elementList]
-data=pd.DataFrame([tagList,levelList,textList, attrList]).T
-data.columns=['tag','level','text', 'attrList']
-data['size']=data['text'].apply(lambda x: len(unicode(x)) if x!=None else 0)
-#data=data.sort_values(by=['size'],ascending=False)
-cDict=data['tag'].value_counts()
+        return result
+
+def getPageList(start,end):
+    pageList=[]
+    count=0
+    for event, obj in etree.iterparse(xml, events=['end']):
+        if obj.tag==fullTag('page'):
+            count+=1
+            if count<start:
+                continue
+            elif count>=start and count<=end:
+                pageList.append(obj)
+            else:
+                break
+    return pageList
 
 
 #%%
 
-read=
+# parse page List to dataFrame
+#pageList=a
+#pageList=[x for x in pageList if x.find(fullTag('ns',prefix='./')).text=='0']
+#tagList=['id','timestamp','title','text'] 
+#result=[]
+#for tag in tagList:
+#    result.append([x.find(fullTag(tag,prefix='.//')).text for x in pageList])
+#data=pd.DataFrame(result).T    
+#data.columns=tagList
 
 
-#%%
-
-def getTagText(data,tagName):
-    for x in data.ix[data['tag']==tag,'text']:
-        print x
-        #%%
-for x in data.ix[data['tag']=='title','text']:
-    print x
-#%%
-title=data.ix[data['tag']=='title',:]
-page
+        
